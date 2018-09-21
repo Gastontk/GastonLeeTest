2d0d638b0ee21214382365e2dd1c2b387d94fda5
Gaston Kennedy <gastontk@hotmail.com>
2018-09-21 08:04:29 -0700

    first commit

---
+++ gastonleetest/src/components/Gaston.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Gaston extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is Gaston's section</h1>
+      </div>
+    )
+  }
+}
---
+++ gastonleetest/src/components/Lee.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Lee extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is Lee's section.</h1>
+      </div>
+    )
+  }
+}
