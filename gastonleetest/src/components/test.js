0bc2cd10244e6afdc819ecd119643a15748b54e4
Gaston Kennedy <gaston@Asus.local>
2018-09-21 11:29:35 -0700

    anohter one

---
+++ gastonleetest/src/components/test.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class test extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a test that was only added to the gastonbranch in git</h1>
+      </div>
+    )
+  }
+}
